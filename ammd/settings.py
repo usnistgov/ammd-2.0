d81dfdb56b458f286d09a048dac2253353f0a798
Adrien Catel <adrien.catel@nist.gov>
2018-07-12 08:16:42 -0400

    feat: ammd theme + homepage implementation
    Change-Id: Id85146471346b5c208f251d871b4b9f7c0a196db

--- ammd/settings.py
+++ ammd/settings.py
@@ -83,7 +83,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                "core_main_app.utils.custom_context_processors.domain_context_processor",  # Needed by any curator app
+                'core_main_app.utils.custom_context_processors.domain_context_processor',  # Needed by any curator app
+                'ammd.custom_context_processors.domain_context_processor',  # Needed by AMMD
             ],
         },
     },
