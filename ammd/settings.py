15a7bc41770e349ade592d50872adcb6a23cd975
Adrien Catel <adrien.catel@nist.gov>
2018-10-25 08:54:07 -0400

    feat: timezone detection settings
    Change-Id: I9e2a9531ab7810f3d18b3ceedaff4581973a95db

--- ammd/settings.py
+++ ammd/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = (
     "rest_framework_swagger",
     "rest_framework_mongoengine",
     "menu",
+    "tz_detect",
 
     # Core apps
     "core_main_app",
@@ -68,6 +69,7 @@ MIDDLEWARE = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'tz_detect.middleware.TimezoneMiddleware',
     # 'password_policies.middleware.PasswordChangeMiddleware',
 )
 
